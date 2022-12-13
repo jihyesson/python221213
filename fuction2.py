98ed9e9a2ef7ea7f4a1af934b7d6f9cef7f94a8e
jihyesson <abcdefg@nate.com>
2022-12-13 13:58:59 +0900

    함수 연습5

--- fuction2.py
+++ fuction2.py
@@ -16,4 +16,32 @@ def swap(x,y):
 print(swap(4,5))
 
 
+print("----불변형식====")
+a = 1.2
+print(id(a))
+a = 2.3
+print(id(a))
+
+print("====가변형식=====")
+lst=[1,2,3]
+print(id(lst))
+lst.append(4)
+print(id(lst))
+
+print("=====이름을 해석하는 순서=====")
+#함수정의
+def func1(a):
+    x=1
+    return x+a
+
+#호출
+print(func1(1))
+
+#함수정의
+x=5
+def func2(a):
+    return x+a
+
+#호출
+print(func2(1))
 
