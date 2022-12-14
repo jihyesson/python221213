0c2a636661efd35124ffb24d6e004056a4dec125
jihyesson <abcdefg@nate.com>
2022-12-14 09:38:04 +0900

    클래스 연습2

--- class2.py
+++ class2.py
@@ -1,9 +1,12 @@
 # class1.py
 # 클래스 정의
 class Person:
+    #클래스 멤버 변수 (일종의 공통 데이터로 사용)
+    num_person = 0
     #초기화 메서드
     def __init__(self):
         self.name = "default name"
+        Person.num_person += 1
     #인스턴스 메서드
     def print(self):
         print("My name is {0}".format(self.name))
@@ -11,9 +14,6 @@ class Person:
 #인스턴스 생성
 p1 = Person()
 p2 = Person()
-p1.name = "손지혜"
-p2.name = "김이슬"
-#에서도 호출
-p1.print()
-p2.print()
+p3 = Person()
+print("인스턴스 갯수:{0}".format(Person.num_person))
 
